a7a1d8b4255e31eac1d0154fbf6b2efaa1889248
LAPTOP-AB8VAKTU\kempin <kempinski.bartosz@gmail.com>
2017-11-15 20:10:48 +0100

    Change template to Asienka

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -9,12 +9,12 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "Asienka, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
-                            String.format(template, "Asiu"));
+                            String.format(template, name));
     }
 }
